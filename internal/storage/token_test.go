a01e88536206f8437d6024e2382b5573f0c5ae4b
Lucas Katayama <492687+lucaskatayama@users.noreply.github.com>
2026-04-29 15:31:07 -0300

    Store token.json in current directory

--- internal/storage/token_test.go
+++ internal/storage/token_test.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/lucaskatayama/oauth2-cli/internal/flow"
@@ -25,5 +27,33 @@ func TestTokenStorage(t *testing.T) {
 
 	if err := s.Delete(); err != nil {
 		t.Fatalf("Delete() error = %v", err)
+	}
+}
+
+func TestTokenStorageDefaultDir(t *testing.T) {
+	tmp := t.TempDir()
+	previous, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("Getwd() error = %v", err)
+	}
+	defer func() {
+		_ = os.Chdir(previous)
+	}()
+	if err := os.Chdir(tmp); err != nil {
+		t.Fatalf("Chdir() error = %v", err)
+	}
+
+	s, err := NewTokenStorage()
+	if err != nil {
+		t.Fatalf("NewTokenStorage() error = %v", err)
+	}
+
+	tok := &flow.Token{AccessToken: "cwd"}
+	if err := s.Save(tok); err != nil {
+		t.Fatalf("Save() error = %v", err)
+	}
+
+	if _, err := os.Stat(filepath.Join(tmp, "token.json")); err != nil {
+		t.Fatalf("token.json not written to cwd: %v", err)
 	}
 }
